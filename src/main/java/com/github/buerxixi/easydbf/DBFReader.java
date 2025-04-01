ab7ead21ec1a7b9e13f40fdebe17c9ea4196730f
刘家强 <liujiaqiang@outlook.com>
2025-04-01 08:13:36 +0800

    modify: 修改reader方法

--- src/main/java/com/github/buerxixi/easydbf/DBFReader.java
+++ src/main/java/com/github/buerxixi/easydbf/DBFReader.java
@@ -1,14 +1,32 @@
 package com.github.buerxixi.easydbf;
 
+import com.github.buerxixi.easydbf.pojo.DBFConstant;
 import com.github.buerxixi.easydbf.pojo.DBFResult;
+import com.github.buerxixi.easydbf.pojo.DBFRow;
+import com.github.buerxixi.easydbf.pojo.DBFRowIterator;
+import lombok.SneakyThrows;
 
+import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.List;
 
-public class DBFReader implements Iterable<List<DBFResult>>{
+public class DBFReader implements Iterable<List<DBFRow>>{
 
+    final private String filename;
+    final private Charset charset;
+
+    DBFReader(String filename, Charset charset) {
+        this.filename = filename;
+        this.charset = charset;
+    }
+
+    DBFReader(String filename) {
+        this(filename, DBFConstant.DEFAULT_CHARSET);
+    }
+
+    @SneakyThrows
     @Override
-    public Iterator<List<DBFResult>> iterator() {
-        return null;
+    public Iterator<List<DBFRow>> iterator() {
+        return new DBFRowIterator(filename, charset);
     }
 }
